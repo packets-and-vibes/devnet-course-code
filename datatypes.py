2d522f52632b9db2826a9c6a14ba5177545eff0a
Kenny Hutto <kenny@devnet.guru>
2026-05-28 21:06:57 -0600

    Added class practice and important functions within classes

--- datatypes.py
+++ datatypes.py
@@ -1,4 +1,4 @@
-from module_6_functions.classes.my_classes import Car
+from module_6_functions.classes.class_folder.my_classes import Car
 
 my_car = Car(make='Ford', model="f150", year=2012, mileage=160000, color='Blue', condition='Used')
 
--- module_6_functions/classes/my_classes.py
+++
@@ -1,8 +0,0 @@
-class Car:
-    def __init__(self, make: str, model: str, year: int, mileage: int, condition: str, color: str) -> None:
-        self.make = make
-        self.model = model
-        self.year = year
-        self.mileage = mileage
-        self.condition = condition
-        self.color = color
